b1669c5ff234dc0e481b60cb208f23fa5c2aa719
Louis Correra <Louis@Correra.org>
2022-12-30 13:19:52 -0700

    Trying to send directory

--- Main.js
+++ Main.js
@@ -4,7 +4,8 @@ const app = express();
 const PORT = 8000;
 
 app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/Response');
+   
 })
 
 const something = {
