43a80a6cffad5777d27a6e859b4b2d5119832f04
Louis Correra <Louis@Correra.org>
2022-12-30 14:00:50 -0700

    added express path

--- Main.js
+++ Main.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8000;
+var path = require('path');
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html');
