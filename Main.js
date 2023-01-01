457753d1713dbf5f4cf64b75a3124a646c6d5e91
Louis Correra <Louis@Correra.org>
2023-01-01 13:49:32 -0700

    Testing connection to UE and testing VaRest plugin. added a put request

--- Main.js
+++ Main.js
@@ -1,25 +1,81 @@
+//Mongo db stuff
+
+const { MongoClient } = require('mongodb');
+
+async function main(){
+    const uri = "mongodb+srv://usersDBAdmin:colorado7@users.p6jfsqo.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect()
+       /** await createListing(client, {
+            userName: "user123",
+            password: "password123",
+            Inventory : [
+                {Weapons: [{Blunt : [{ItemName: "Axe", ItemWeight: 50, Description: "And my Axe!"}, {ItemName: "Axe", ItemWeight: 50, Description: "And my Axe!"}, {ItemName: "Axe", ItemWeight: 50, Description: "And my Axe!"}]}, {Sharp : [{ItemName: "Sword", ItemWeight: 50, Description: "Swing!"}, {ItemName: "Sword", ItemWeight: 50, Description: "Swing!"}, {ItemName: "Sword", ItemWeight: 50, Description: "Swing!"}, {ItemName: "Sword", ItemWeight: 50, Description: "Swing!"}]}]},
+                {Treasure: [ {ItemName: "unknown", ItemWorth: 0, Description: "Shiny shiny"}, {ItemName: "unknown", ItemWorth: 0, Description: "Shiny shiny"}, {ItemName: "unknown", ItemWorth: 0, Description: "Shiny shiny"}, {ItemName: "unknown", ItemWorth: 0, Description: "Shiny shiny"}]},
+                {QuestItems: [{QuestName: "QuestName", QuestWayPoint: [100, 200, 300], Description:"This is a quest"}, {QuestName: "QuestName", QuestWayPoint: [100, 200, 300], Description:"This is a quest"}, {QuestName: "QuestName", QuestWayPoint: [100, 200, 300], Description:"This is a quest"}, {QuestName: "QuestName", QuestWayPoint: [100, 200, 300], Description:"This is a quest"}]}
+            ]
+        }) */
+
+        //[{Weapons: [{key: value}, {}, {}]}, {Treasure:[{}, {}, {}]}, {[{}, {}, {}]}]
+
+       // await updateListing(client, "user1234", {userName: "user1235"});
+        //await listDatabases(client);
+    } catch (e) {
+        console.error(e)
+    }finally{
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function createListing(client, newListing){
+   const result = await client.db("userAccounts").collection("users").insertOne(newListing);
+}
+
+async function updateListing(client, listingToUpdate, updateListing){
+    const result = await client.db("userAccounts").collection("users").updateOne({userName: listingToUpdate }, {$set: updateListing});
+   
+    console.log(`${result.matchedCount} documents were found`);
+    console.log(`${result.modifiedCount} were updated`);
+}
+
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases");
+    databasesList.databases.forEach(element => {
+        console.log(`- ${element.name}`);
+    });
+}
+
+//Node API stuff
+
 const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8000;
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
 
 app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html');
     
 })
 
-const something = {
-    'age': 30,
-    'name': 'dirty',
-    'location': 'flirty'
-
-    //
-}
 
 app.get('/api', (request, response)=>{
-    response.json(something);
+   // response.json();
+})
+
+app.post('/api/:user', (req, res) =>{
+
+})
+
+app.put('/api/:user', (res, req)=>{
+    console.log(res.body);
+    //updateListing(client, req.params.user, {userName: res.body});
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
