82d048d9ad8ba0339f605beaade065648f096e12
Louis Correra <Louis@Correra.org>
2023-01-01 21:42:26 -0700

    End of UpdateListing not being reached

--- Main.js
+++ Main.js
@@ -38,8 +38,9 @@ async function createListing(client, newListing){
 
 async function updateListing(client, listingToUpdate, updateListing){
     console.log("Called updateListing");
-    console.log(client);
+    //console.log(client);
     await client.connect();
+    console.log("Called updateListing");
     const result = await client.db("userAccounts").collection("users").updateOne({userName: listingToUpdate }, {$set: updateListing});
     console.log("tried to update");
     console.log(`${result.matchedCount} documents were found`);
