6826d0674ecae617ba3daf3e037268f4f11ffbda
Louis Correra <Louis@Correra.org>
2023-01-06 15:51:31 -0700

    testing json parser

--- user-schema.js
+++ user-schema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     username: String,
     password: String,
-    inventory : [{name: String, description: String, class: Number, weight: Number, value: Number}]
+    inventory : [{Item: Object}]
 })
 
 module.exports = mongoose.model('users', userSchema);
